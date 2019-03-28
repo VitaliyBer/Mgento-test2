89613f2fd547614eabfddd1483ae66714cb6bb0b
Vitaliy <bernik@i.ua>
2019-03-28 12:46:17 +0200

    Catalog_Product_widget_qty

---
+++ design/frontend/Training/scout/web/js/qty.js
@@ -0,0 +1,46 @@
+define([
+    'jquery',
+    'underscore',
+    'jquery/ui',
+    'knockout',
+    'domReady!'
+], function ($, _) {
+    'use strict';
+
+    $.widget('mage.qty', {
+
+
+        _create: function () {
+            let widgetElement = this.element;
+
+
+            this._on('.cart__minus', {
+                click: function (e) {
+                    e.preventDefault();
+                    let widgetValue = parseInt($('#qty').val());
+                    if (!isNaN(widgetValue) && widgetValue > 1) {
+                        $('#qty').val(widgetValue - 1);
+                    } else {
+                        $('#qty').val(1);
+                    }
+                }
+            })
+
+            this._on('.cart__plus', {
+                click: function (e) {
+                    e.preventDefault();
+                    let widgetValue = parseInt($('#qty').val());
+                    if (!isNaN(widgetValue)) {
+                        $('#qty').val(widgetValue+1);
+                    } else {
+                        $('#qty').val(1);
+                    }
+                }
+            })
+        }
+    })
+
+
+    return $.mage.qyt;
+
+});
