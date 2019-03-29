6f4c80ca526d5781c64449948d7c150ca8dce686
Vitaliy <bernik@i.ua>
2019-03-29 16:06:10 +0200

    Catalog_Product_fixed_bag

--- design/frontend/Training/scout/web/js/qty.js
+++ design/frontend/Training/scout/web/js/qty.js
@@ -8,39 +8,37 @@ define([
     'use strict';
 
     $.widget('mage.qty', {
-
-
         _create: function () {
             let widgetElement = this.element;
+            let widgetValue = parseInt($('#qty').val());
+            let valueDefault = $('#qty').val(1);
 
 
             this._on('.cart__minus', {
                 click: function (e) {
                     e.preventDefault();
-                    let widgetValue = parseInt($('#qty').val());
                     if (!isNaN(widgetValue) && widgetValue > 1) {
                         $('#qty').val(widgetValue - 1);
                     } else {
-                        $('#qty').val(1);
+                        valueDefault;
                     }
                 }
-            })
+            });
 
             this._on('.cart__plus', {
                 click: function (e) {
                     e.preventDefault();
-                    let widgetValue = parseInt($('#qty').val());
                     if (!isNaN(widgetValue)) {
-                        $('#qty').val(widgetValue+1);
+                        $('#qty').val(widgetValue + 1);
                     } else {
-                        $('#qty').val(1);
+                        valueDefault;
                     }
                 }
             })
         }
-    })
+    });
 
 
-    return $.mage.qyt;
+    return $.mage.qty;
 
 });
