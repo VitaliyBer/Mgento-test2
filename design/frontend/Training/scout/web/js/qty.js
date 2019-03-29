4cba362a30c8603dd1e7c11579dd33ae1aaf53ed
Vitaliy <bernik@i.ua>
2019-03-29 17:15:03 +0200

    Catalog_Product_fixed_bag

--- design/frontend/Training/scout/web/js/qty.js
+++ design/frontend/Training/scout/web/js/qty.js
@@ -8,32 +8,39 @@ define([
     'use strict';
 
     $.widget('mage.qty', {
+
         _create: function () {
 
             this._on('.cart__minus', {
-                click: function (e) {
-                    e.preventDefault();
-                    let widgetValue = parseInt($('#qty').val());
-                    if (!isNaN(widgetValue) && widgetValue > 1) {
-                        $('#qty').val(widgetValue - 1);
-                    } else {
-                        $('#qty').val(1);
-                    }
-                }
+                click: '_cartMinus'
             });
 
             this._on('.cart__plus', {
-                click: function (e) {
-                    e.preventDefault();
-                    let widgetValue = parseInt($('#qty').val());
-                    if (!isNaN(widgetValue)) {
-                        $('#qty').val(widgetValue + 1);
-                    } else {
-                        $('#qty').val(1);
-                    }
-                }
+                click: '_cartPlus'
             })
+        },
+
+
+        _cartMinus: function (e) {
+            e.preventDefault();
+            let widgetValue = parseInt($('#qty').val());
+            if (!isNaN(widgetValue) && widgetValue > 1) {
+                $('#qty').val(widgetValue - 1);
+            } else {
+                $('#qty').val(1);
+            }
+        },
+
+        _cartPlus: function (e) {
+            e.preventDefault();
+            let widgetValue = parseInt($('#qty').val());
+            if (!isNaN(widgetValue)) {
+                $('#qty').val(widgetValue + 1);
+            } else {
+                $('#qty').val(1);
+            }
         }
+
     });
 
 
--- design/frontend/Training/scout/web/js/scrollCart.js
+++ design/frontend/Training/scout/web/js/scrollCart.js
@@ -13,30 +13,34 @@ define([
         _create: function () {
 
             this._on('#link__details', {
-                click: function (e) {
-                    e.preventDefault();
-                    let id = $(".description_block .product.attribute.description"),
-                        top = $(id).offset().top;
-                    $('body, html').animate({scrollTop: top }, 1000);
+                click: '_linkScrollDetails'
 
-                    $('.description__link-items li').removeClass("active__link")
-                    $('#link__details').addClass('active__link')
-
-                }
-            })
+            });
 
             this._on('#link__reviews', {
-                click: function (e) {
-                    e.preventDefault();
-                    let id = $(".fieldset.review-fieldset"),
-                        top = $(id).offset().top;
-                    $('body, html').animate({scrollTop: top}, 700);
-
-                    $('.description__link-items li').removeClass("active__link")
-                    $('#link__reviews').addClass('active__link')
-
-                }
+                click: '_linkScrollReviews'
             })
+        },
+
+        _linkScrollDetails: function (e) {
+            e.preventDefault();
+            let id = $(".description_block .product.attribute.description"),
+                top = $(id).offset().top;
+            $('body, html').animate({scrollTop: top}, 1000);
+
+            $('.description__link-items li').removeClass("active__link");
+            $('#link__details').addClass('active__link')
+        },
+
+        _linkScrollReviews: function (e) {
+            e.preventDefault();
+            let id = $(".fieldset.review-fieldset"),
+                top = $(id).offset().top;
+            $('body, html').animate({scrollTop: top}, 700);
+
+            $('.description__link-items li').removeClass("active__link");
+            $('#link__reviews').addClass('active__link')
+
         }
     })
 
